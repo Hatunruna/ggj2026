c6cd5058f0c84a22729f869f71fe3bebf7249290
Julien Bernard <julien.bernard@univ-fcomte.fr>
2026-01-31 13:57:52 +0100

    update wang sets

--- data/map/walls.tsx
+++ data/map/walls.tsx
@@ -2,8 +2,10 @@
 <tileset version="1.10" tiledversion="1.10.2" name="walls" tilewidth="128" tileheight="128" tilecount="64" columns="8">
  <image source="walls.png" width="1024" height="1024"/>
  <wangsets>
-  <wangset name="Red walls" type="edge" tile="-1">
+  <wangset name="Walls" type="edge" tile="-1">
    <wangcolor name="Red walls" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Green Walls" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Blue Walls" color="#0000ff" tile="-1" probability="1"/>
    <wangtile tileid="1" wangid="1,0,0,0,0,0,0,0"/>
    <wangtile tileid="2" wangid="0,0,1,0,0,0,0,0"/>
    <wangtile tileid="3" wangid="1,0,1,0,0,0,0,0"/>
@@ -19,38 +21,32 @@
    <wangtile tileid="13" wangid="1,0,0,0,1,0,1,0"/>
    <wangtile tileid="14" wangid="0,0,1,0,1,0,1,0"/>
    <wangtile tileid="15" wangid="1,0,1,0,1,0,1,0"/>
-  </wangset>
-  <wangset name="Green walls" type="edge" tile="-1">
-   <wangcolor name="Green walls" color="#00ff00" tile="-1" probability="1"/>
-   <wangtile tileid="17" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="18" wangid="0,0,1,0,0,0,0,0"/>
-   <wangtile tileid="19" wangid="1,0,1,0,0,0,0,0"/>
-   <wangtile tileid="20" wangid="0,0,0,0,1,0,0,0"/>
-   <wangtile tileid="21" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="22" wangid="0,0,1,0,1,0,0,0"/>
-   <wangtile tileid="24" wangid="0,0,0,0,0,0,1,0"/>
-   <wangtile tileid="25" wangid="1,0,0,0,0,0,1,0"/>
-   <wangtile tileid="26" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="27" wangid="1,0,1,0,0,0,1,0"/>
-   <wangtile tileid="28" wangid="0,0,0,0,1,0,1,0"/>
-   <wangtile tileid="29" wangid="1,0,0,0,1,0,1,0"/>
-   <wangtile tileid="30" wangid="0,0,1,0,1,0,1,0"/>
-  </wangset>
-  <wangset name="Blue walls" type="edge" tile="-1">
-   <wangcolor name="Blue walls" color="#0000ff" tile="-1" probability="1"/>
-   <wangtile tileid="33" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="34" wangid="0,0,1,0,0,0,0,0"/>
-   <wangtile tileid="35" wangid="1,0,1,0,0,0,0,0"/>
-   <wangtile tileid="36" wangid="0,0,0,0,1,0,0,0"/>
-   <wangtile tileid="37" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="38" wangid="0,0,1,0,1,0,0,0"/>
-   <wangtile tileid="40" wangid="0,0,0,0,0,0,1,0"/>
-   <wangtile tileid="41" wangid="1,0,0,0,0,0,1,0"/>
-   <wangtile tileid="42" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="43" wangid="1,0,1,0,0,0,1,0"/>
-   <wangtile tileid="44" wangid="0,0,0,0,1,0,1,0"/>
-   <wangtile tileid="45" wangid="1,0,0,0,1,0,1,0"/>
-   <wangtile tileid="46" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="17" wangid="2,0,0,0,0,0,0,0"/>
+   <wangtile tileid="18" wangid="0,0,2,0,0,0,0,0"/>
+   <wangtile tileid="19" wangid="2,0,2,0,0,0,0,0"/>
+   <wangtile tileid="20" wangid="0,0,0,0,2,0,0,0"/>
+   <wangtile tileid="21" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="22" wangid="0,0,2,0,2,0,0,0"/>
+   <wangtile tileid="24" wangid="0,0,0,0,0,0,2,0"/>
+   <wangtile tileid="25" wangid="2,0,0,0,0,0,2,0"/>
+   <wangtile tileid="26" wangid="0,0,2,0,0,0,2,0"/>
+   <wangtile tileid="27" wangid="2,0,2,0,0,0,2,0"/>
+   <wangtile tileid="28" wangid="0,0,0,0,2,0,2,0"/>
+   <wangtile tileid="29" wangid="2,0,0,0,2,0,2,0"/>
+   <wangtile tileid="30" wangid="0,0,2,0,2,0,2,0"/>
+   <wangtile tileid="33" wangid="3,0,0,0,0,0,0,0"/>
+   <wangtile tileid="34" wangid="0,0,3,0,0,0,0,0"/>
+   <wangtile tileid="35" wangid="3,0,3,0,0,0,0,0"/>
+   <wangtile tileid="36" wangid="0,0,0,0,3,0,0,0"/>
+   <wangtile tileid="37" wangid="3,0,0,0,3,0,0,0"/>
+   <wangtile tileid="38" wangid="0,0,3,0,3,0,0,0"/>
+   <wangtile tileid="40" wangid="0,0,0,0,0,0,3,0"/>
+   <wangtile tileid="41" wangid="3,0,0,0,0,0,3,0"/>
+   <wangtile tileid="42" wangid="0,0,3,0,0,0,3,0"/>
+   <wangtile tileid="43" wangid="3,0,3,0,0,0,3,0"/>
+   <wangtile tileid="44" wangid="0,0,0,0,3,0,3,0"/>
+   <wangtile tileid="45" wangid="3,0,0,0,3,0,3,0"/>
+   <wangtile tileid="46" wangid="0,0,3,0,3,0,3,0"/>
   </wangset>
  </wangsets>
 </tileset>
