9e20dcc1a216ac1d7478ef8753a03fa4efe85e20
Julien Bernard <julien.bernard@univ-fcomte.fr>
2026-01-31 13:54:55 +0100

    update walls tileset

--- data/map/walls.tsx
+++ data/map/walls.tsx
@@ -3,7 +3,7 @@
  <image source="walls.png" width="1024" height="1024"/>
  <wangsets>
   <wangset name="Red walls" type="edge" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Red walls" color="#ff0000" tile="-1" probability="1"/>
    <wangtile tileid="1" wangid="1,0,0,0,0,0,0,0"/>
    <wangtile tileid="2" wangid="0,0,1,0,0,0,0,0"/>
    <wangtile tileid="3" wangid="1,0,1,0,0,0,0,0"/>
@@ -19,6 +19,38 @@
    <wangtile tileid="13" wangid="1,0,0,0,1,0,1,0"/>
    <wangtile tileid="14" wangid="0,0,1,0,1,0,1,0"/>
    <wangtile tileid="15" wangid="1,0,1,0,1,0,1,0"/>
+  </wangset>
+  <wangset name="Green walls" type="edge" tile="-1">
+   <wangcolor name="Green walls" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="17" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="18" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="19" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="20" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="21" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="22" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="24" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="25" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="26" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="27" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="28" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="29" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="30" wangid="0,0,1,0,1,0,1,0"/>
+  </wangset>
+  <wangset name="Blue walls" type="edge" tile="-1">
+   <wangcolor name="Blue walls" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="33" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="34" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="35" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="36" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="37" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="38" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="40" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="41" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="42" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="43" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="44" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="45" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="46" wangid="0,0,1,0,1,0,1,0"/>
   </wangset>
  </wangsets>
 </tileset>
--- raw/walls/make-spritesheet.sh
+++ raw/walls/make-spritesheet.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-montage "$SCRIPT_DIR/1-*.png" -background none -tile 8x8 -geometry 128x128 "png32:$SCRIPT_DIR/../../data/map/walls.png"
+montage "$SCRIPT_DIR/1-*.png" "$SCRIPT_DIR/2-*.png" "$SCRIPT_DIR/3-*.png" -background none -tile 8x8 -geometry 128x128 "png32:$SCRIPT_DIR/../../data/map/walls.png"
